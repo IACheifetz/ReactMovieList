102415582dff1f01345abaed4aef88adbe96b01a
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-05 12:14:07 -0700

    began app js declared state and began first function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import MovieForm from './MovieForm';
+import MovieList from './MovieList';
+import Movie from './Movie';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState(null);
+  const [currentMovie, setCurrentMovie] = useState({ name: '', director: '', releaseDate: '', color: '' });
+  
+  function submitMovie(movie) {
+    const newMovie = {
+      ...movie,
+      id: Math.ceil(Math.random() * 9999999),
+    };
+    setAllMovies([...allMovies, newMovie]);
+  }
+
 }
 
 export default App;
