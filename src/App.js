c698d0028c18348a348d9ca8a515a726374fb480
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-05 12:19:02 -0700

    added filter and delete functions and began adding to return

--- src/App.js
+++ src/App.js
@@ -17,6 +17,29 @@ function App() {
     setAllMovies([...allMovies, newMovie]);
   }
 
+  function handleDeleteMovie(name) {
+    const movieIndex = allMovies.findIndex(movie => movie.name === name);
+    allMovies.splice(movieIndex, 1);
+    setAllMovies([...allMovies]);
+  }
+
+  function handleFilterMovies(search) {
+    const lessMovies = allMovies.filter(movie => movie.name.includes(search));
+    search ? setFilteredMovies(lessMovies) : setFilteredMovies(allMovies);
+  }
+
+
+  return (
+    <div className="App">
+      <div className='current-movie quarter'>
+        <Movie movie={currentMovie}/>
+      </div>
+      <div className='movie-filter quarter'>
+        Filter Titles
+        <input onChange={(e) => handleFilterMovies(e.target.value)} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
